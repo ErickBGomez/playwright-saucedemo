2cb78971e5fd213a41737db3639e7385ba17dcde
ErickBGomez <erickbgomez.dev@gmail.com>
2025-07-05 13:07:56 -0500

    Define login elements to test in pages folder

---
+++ pages/login.page.ts
@@ -0,0 +1,42 @@
+import { Page, Locator } from "@playwright/test";
+
+export class LoginPage {
+  readonly page: Page;
+  readonly usernameInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+  readonly errorMessage: Locator;
+  readonly loginLogo: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameInput = page.locator('[data-test="username"]');
+    this.passwordInput = page.locator('[data-test="password"]');
+    this.loginButton = page.locator('[data-test="login-button"]');
+    this.errorMessage = page.locator('[data-test="error"]');
+    this.loginLogo = page.locator(".login_logo");
+  }
+
+  async goto() {
+    await this.page.goto("index.html");
+  }
+
+  async login(username: string, password: string) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+
+  async clearFields() {
+    await this.usernameInput.clear();
+    await this.passwordInput.clear();
+  }
+
+  async getErrorMessage() {
+    return await this.errorMessage.textContent();
+  }
+
+  async isErrorVisible() {
+    return await this.errorMessage.isVisible();
+  }
+}
