6af756a2f20bb4ddeea0d22972af96bf7d827cc9
ErickBGomez <erickbgomez.dev@gmail.com>
2025-07-06 13:01:59 -0600

    Load valid and invalid credentials to login UI tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/login.page";
 import { InventoryPage } from "../pages/inventory.page";
+import { config } from "../utils/config";
 
 test.describe("Login Tests", () => {
   let loginPage: LoginPage;
@@ -14,26 +15,11 @@ test.describe("Login Tests", () => {
   });
 
   test.describe("Successful Login", () => {
-    const validCredentials = [
-      {
-        username: "standard_user",
-        password: "secret_sauce",
-      },
-      {
-        username: "problem_user",
-        password: "secret_sauce",
-      },
-      {
-        username: "performance_glitch_user",
-        password: "secret_sauce",
-      },
-    ];
-
     // For each valid user saved in this array, create a test
     // Even tough problem_user and performance_glitch_user are not standard users,
     // these can indeed login successfully
     // Approach made by the official documentation: https://playwright.dev/docs/test-parameterize#parameterized-tests
-    validCredentials.forEach(({ username, password }) => {
+    config.validCredentials.forEach(({ username, password }) => {
       test(`should login successfully with ${username}`, async ({ page }) => {
         await loginPage.login(username, password);
         await expect(page.url()).toContain("/inventory.html");
@@ -42,26 +28,11 @@ test.describe("Login Tests", () => {
   });
 
   test.describe("Failed Login", () => {
-    const invalidUsers = [
-      {
-        username: "locked_out_user",
-        password: "secret_sauce",
-      },
-      {
-        username: "invalid_user",
-        password: "invalid_password",
-      },
-      {
-        username: "",
-        password: "",
-      },
-    ];
-
     // Now with invalid users
     // locked_out_user is considered as invalid because it cannot
     // login to the page. I also considered invalid_user as a invented
     // user that does not exist in the system, and also i added blank credentials values.
-    invalidUsers.forEach(({ username, password }) => {
+    config.invalidCredentials.forEach(({ username, password }) => {
       test(`should show error for invalid credentials: ${username}`, async () => {
         await loginPage.login(username, password);
         await expect(loginPage.errorMessage).toBeVisible();
